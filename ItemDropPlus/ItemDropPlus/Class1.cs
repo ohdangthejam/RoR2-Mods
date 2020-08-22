3854ae683d80ced5cb547e721ccf9d45a730d382
Oh Dang The Jam <ohdangthejam@gmail.com>
2020-08-22 11:10:05 -0600

    ItemDropPlus initial commit

--- ItemDropPlus/ItemDropPlus/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;  
-
-namespace ItemDropPlus
-{
-    public class Class1
-    {
-    }
-}
---
+++ ItemDropPlus/ItemDropPlus/ItemDropPlus.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using RoR2;
+
+namespace OhDangTheJam
+{
+    [BepInDependency("com.bepis.r2api")]
+    //Change these
+    [BepInPlugin("com.OhDangTheJam.ItemDropPlus", "ItemDropPlus", "1.0.0")]
+    public class ItemDropPlus : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Chat.AddMessage("ITEM DROP PLUS ACTIVATED");
+            On.EntityStates.Huntress.ArrowRain.OnEnter += (orig, self) =>
+            {
+                // [The code we want to run]
+                Chat.AddMessage("You used Huntress's Arrow Rain!");
+
+                // Call the original function (orig)
+                // on the object it's normally called on (self)
+                orig(self);
+            };
+        }
+    }
+}
